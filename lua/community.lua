71ca4505fdda2ca9b1ef3dbd9577295be7aa5dfd
Denis Golod <golod@astera.in>
2024-05-14 17:00:45 +0300

    initial commit

---
+++ lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- Plugins
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- Packs
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.lua" },
+}
