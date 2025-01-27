d1ef2fdd3968760e65f11f5562f261c0a7dd623e
Denis <golod@astera.in>
2025-01-28 01:19:52 +0200

    Update

--- lua/community.lua
+++ lua/community.lua
@@ -8,14 +8,16 @@ return {
   -- Plugins
   { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.search.nvim-spectre" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.recipes.neovide" },
   -- Packs
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.search.nvim-spectre" },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.pack.terraform" },
 }
