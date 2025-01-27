254de611636060c146c61cb42b302b446e0fe0b4
Denis <golod@astera.in>
2025-01-28 01:21:43 +0200

    Apply some sort

--- lua/community.lua
+++ lua/community.lua
@@ -7,17 +7,17 @@ return {
   "AstroNvim/astrocommunity",
   -- Plugins
   { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.recipes.neovide" },
+  { import = "astrocommunity.search.nvim-spectre" },
   -- Packs
-  { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.terraform" },
 }
