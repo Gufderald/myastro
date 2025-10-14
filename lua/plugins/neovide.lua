ec64ff17dab0281fc4def27c163a43447d3760e7
Denis <denis.golod.w@gmail.com>
2025-10-14 19:03:15 +0300

    Update configuration

---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,61 @@
+if not vim.g.neovide then
+  return {} -- do nothing if not in a Neovide session
+end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        -- paste
+        ["<C-S-v>"] = { '"+p', desc = "Paste" },
+        ["XF86Paste"] = { '"+p', desc = "Paste" },
+        ["<S-Insert>"] = { '"+p', desc = "Paste" },
+      },
+      i = {
+        -- setting a mapping to false will disable it
+        ["<C-S-v>"] = { "<C-r>+", desc = "Paste" },
+        ["XF86Paste"] = { "<C-r>+", desc = "Paste" },
+        ["<S-Insert>"] = { "<C-r>+", desc = "Paste" },
+        -- ["<esc>"] = false,
+      },
+      x = {
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        -- paste
+        ["<C-S-v>"] = { '"+p', desc = "Paste" },
+        ["XF86Paste"] = { '"+p', desc = "Paste" },
+        ["<S-Insert>"] = { '"+p', desc = "Paste" },
+      },
+      c = {
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        -- paste
+        ["<C-S-v>"] = { "<C-r>+", desc = "Paste" },
+        ["XF86Paste"] = { "<C-r>+", desc = "Paste" },
+        ["<S-Insert>"] = { "<C-r>+", desc = "Paste" },
+      },
+    },
+    options = {
+      opt = { -- configure vim.opt options
+        tabstop = 2, -- A tab character will be displayed as 2 spaces
+        shiftwidth = 2, -- Indent by 2 spaces when using auto-indent or >>/<<
+        expandtab = true, -- Insert spaces instead of actual tab characters when pressing Tab
+        softtabstop = 2, -- Makes the Tab and Backspace keys work with 2-space increments
+        smartindent = true, -- Enable smart auto-indentation for new lines
+        -- configure font
+        guifont = "JetBrainsMono Nerd Font:h16",
+        -- line spacing
+        linespace = 0,
+      },
+
+      g = { -- configure vim.g variables
+        neovide_fullscreen = false,
+      },
+    },
+  },
+}
--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -18,4 +18,25 @@ return {
       },
     },
   },
+  -- {
+  --   "yetone/avante.nvim",
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     provider = "openai",
+  --     providers = {
+  --       openai = {
+  --         endpoint = "https://api.openai.com/v1",
+  --         api_key_name = "cmd:kwallet-query kdewallet -r openai_key",
+  --         model = "gpt-4.1-mini", -- your desired model (or use gpt-4o, etc.)
+  --         extra_request_body = {
+  --           -- timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+  --           temperature = 0.75,
+  --           max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+  --           --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
